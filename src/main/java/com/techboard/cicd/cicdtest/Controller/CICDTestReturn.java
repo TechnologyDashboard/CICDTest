ea39f165c500bc153e2ecf03b903011c33025935
Pradipto <you@example.com>
2021-08-09 20:57:11 +0530

    initial commit

---
+++ src/main/java/com/techboard/cicd/cicdtest/Controller/CICDTestReturn.java
@@ -0,0 +1,14 @@
+package com.techboard.cicd.cicdtest.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CICDTestReturn {
+
+    @GetMapping(value = "/cicdtest")
+    public ResponseEntity sayHello(){
+        return ResponseEntity.ok().body("hello for CICD");
+    }
+}
